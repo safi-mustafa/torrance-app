df1f1c5894e8215bb17ee71515aea38b9e7318f3
thisisirfan <irfansaddress@gmail.com>
2023-01-18 16:50:19 +0500

    master changed to js version

---
+++ app-styles.jsx
@@ -0,0 +1,55 @@
+import { StyleSheet, StyleProp } from "react-native";
+
+const defaultFontSize = 16;
+const appStyles = StyleSheet.create({
+  alignLeft: {
+    textAlign: "left",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  fw500: {
+    fontWeight: "500",
+  },
+  p: {
+    fontSize: defaultFontSize,
+  },
+  h1: {
+    fontSize: defaultFontSize * 2.5,
+  },
+  h2: {
+    fontSize: defaultFontSize * 2,
+  },
+  h3: {
+    fontSize: defaultFontSize * 1.8,
+  },
+  h4: {
+    fontSize: defaultFontSize * 1.4,
+  },
+  h5: {
+    fontSize: defaultFontSize * 1.2,
+  },
+  my1:{
+    marginVertical: 2
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  clear: {
+    backgroundColor: "transparent",
+    margin: 0,
+    padding: 0,
+  }
+});
+export default appStyles;
---
+++ components/DashButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image, TouchableOpacity, Text, View  } from "react-native";
+
+import appStyles from "../app-styles";
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+
+export function DashButton({
+  title = "",
+  subtitle = "",
+  style = {},
+  icon = "",
+  ...otherProps
+}) {
+  const colorScheme = useColorScheme();
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: Colors[colorScheme].background,
+        padding: 15,
+        borderRadius: 6,
+        width: "100%",
+        ...style,
+      }}
+      {...otherProps}
+    >
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        {icon && <Image style={{height: 70, width: 70, marginRight: 20}} source={icon} />}
+        <View>
+          <Text style={{ color: "#ccc" }}>Quick Submit</Text>
+          <Text style={[appStyles.h5, appStyles.fw500, appStyles.my1]}>
+            {title}
+          </Text>
+          <Text style={{ color: "#999" }}>{subtitle}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
