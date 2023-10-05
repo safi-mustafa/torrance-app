6369d612e81cef51b273e7752d2fae3f59f05a21
thisisirfan <irfansaddress@gmail.com>
2023-10-05 20:16:53 +0500

    live credentials added

--- constants/Misc.js
+++ constants/Misc.js
@@ -1,10 +1,10 @@
-export const HOST_URL = 'http://203.124.35.18:8402';
-export const BASE_URL = `${HOST_URL}/api`;
-export const STORAGE_URL = 'http://203.124.35.18:8403';
-
-// export const HOST_URL = 'https://torranceapi.eztrak.net';
+// export const HOST_URL = 'http://203.124.35.18:8402';
 // export const BASE_URL = `${HOST_URL}/api`;
-// export const STORAGE_URL = 'https://torrance.eztrak.net';
+// export const STORAGE_URL = 'http://203.124.35.18:8403';
+
+export const HOST_URL = 'https://torranceapi.eztrak.net';
+export const BASE_URL = `${HOST_URL}/api`;
+export const STORAGE_URL = 'https://torrance.eztrak.net';
 
 export const DATE_TIME_MODE = {
     DATE: 'date',
