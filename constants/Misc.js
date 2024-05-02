90e3042c8259f7e441f64225f83f199fa0bff443
anam <anam.k51014@gmail.com>
2024-05-02 16:12:56 +0500

    optimization branch

--- constants/Misc.js
+++ constants/Misc.js
@@ -1,8 +1,8 @@
-// export const BASE_URL = 'http://203.124.35.18:8402/api';
-// export const STORAGE_URL = 'http://203.124.35.18:8403';
+export const BASE_URL = 'http://203.124.35.18:8402/api';
+export const STORAGE_URL = 'http://203.124.35.18:8403';
 
-export const BASE_URL = 'https://torrancemobile.eliteprojectmanagementllc.com/api';
-export const STORAGE_URL = 'https://torrance.eliteprojectmanagementllc.com';
+// export const BASE_URL = 'https://torrancemobile.eliteprojectmanagementllc.com/api';
+// export const STORAGE_URL = 'https://torrance.eliteprojectmanagementllc.com';
 
 export const DATE_TIME_MODE = {
     DATE: 'date',
